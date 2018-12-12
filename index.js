912689e8711c0cc98cb6b2d8e0a1ce02c4e26ecb
William Suarez <wilisumo@gmail.com>
2018-12-12 03:41:13 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
-// add solution here
+function theBeatlesPlay(musicians,instruments){
+  var array = [];
+  
+  for(var i=0;i< musicians.length ; i++){
+    var cadena = musicians[i] + " plays " + instruments[i]; 
+    array.push(cadena);
+  }
+  return array;
+}
+
+function johnLennonFacts(facts){
+  var contador = (facts.length) - 1 ;
+  console.log('contador ' + contador);
+  var array = [];
+  
+  while(contador => 0) {
+    var cadena = facts[contador] + "!!!";
+    console.log('cadena' + cadena);
+    array[contador] = cadena;
+    contador--;
+  }
+  return array;
+}
+
+function iLoveTheBeatles(number){
+  
+  var array = [];
+  
+  do{
+    
+    array[contador] = "I love the Beatles!";
+    
+    number++;
+  }while(number < 15);
+   return array;
+}
